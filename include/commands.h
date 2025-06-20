0ca1bdbaab83135c1a4943d183964e90d5cdb081
Gabriel Guerrero Maldonado <gabriel.guemal2010@gmail.com>
2025-06-20 10:05:49 -0500

    Added .h files

---
+++ include/commands.h
@@ -0,0 +1,10 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+void handleCommand(const std::string& input, std::string& username);
+
+#endif
